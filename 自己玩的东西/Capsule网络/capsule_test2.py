6e20521d3aa6f1ae98cedc2e19b36a66c104684c
ccaipc <395871987@qq.com>
2018-06-12 16:16:02 +0800

    batch模式routing,卷积网络的实现

--- 自己玩的东西/Capsule网络/capsule_test2.py
+++ 自己玩的东西/Capsule网络/capsule_test2.py
@@ -30,7 +30,7 @@ size_x = 3
 size_y = 3
 
 # 根据卷积参数计算的输出的大小
-v_x, v_y = 4, 4
+v_x, v_y = 4, 4  # 暂时是手动，以后需要自动化计算出来，VALID模式
 v_channel_num = 16
 v_capsule_num = 9
 
@@ -40,15 +40,23 @@ W = get_weight(shape=(size_x, size_y, u_channel_num, v_channel_num, u_capsule_nu
 # 先用笨办法实现，有待用更好的concat方法实现的更加紧凑
 for v_channel_num_i in range(v_channel_num):
     W_temp = W[:, :, :, v_channel_num_i, :, :]
+    u_predict_y = []
     for v_y_i in range(v_y):
+        u_predict_x = []
         for v_x_i in range(v_x):
-            u_temp = u[:, v_x_i:v_x_i+size_x, v_y_i:v_y_i+size_y, :]
+            u_temp = u[:, v_x_i:v_x_i+size_x, v_y_i:v_y_i+size_y, :, :]
             u_temp = tf.expand_dims(u_temp, axis=5)
             u_predict_temp = tf.reduce_sum(u_temp * W_temp, axis=4)
             u_predict_temp = tf.expand_dims(u_predict_temp, axis=-1)
-            u_predict.append(u_predict_temp)  # 暂时先都堆在一起，然后要进行reshape
-# 临时办法，尽量不要用reshape
-u_predict = tf.reshape(tf.concat(u_predict, axis=-1), (-1, size_x, size_y, u_channel_num, v_capsule_num, v_x, v_y, v_channel_num))
+            u_predict_x.append(u_predict_temp)
+        u_predict_x = tf.concat(u_predict_x, axis=-1)
+        u_predict_x = tf.expand_dims(u_predict_x, axis=-1)
+        u_predict_y.append(u_predict_x)
+    u_predict_y = tf.concat(u_predict_y, axis=-1)
+    u_predict_y = tf.expand_dims(u_predict_y, axis=-1)
+    u_predict.append(u_predict_y)
+u_predict = tf.concat(u_predict, axis=-1)
+print(u_predict.get_shape())
 
 # routing参数计算
 b = tf.constant(0.0, shape=(size_x, size_y, u_channel_num, v_x, v_y, v_channel_num))
