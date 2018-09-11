d6ab19f95d2aeb4fca5f86b70bd549381d3295fa
ccaipc <395871987@qq.com>
2018-09-11 11:46:52 +0800

    上传模型论文

--- PyTorch实现模板/全连接网络.py
+++ PyTorch实现模板/全连接网络.py
@@ -5,23 +5,53 @@ import numpy as np
 
 
 # 全连接网络
-class FullConnectedNet(t.nn.Module):
-    def __init__(self):
+class FullConnectedNetV1(t.nn.Module):
+    def __init__(self):  # 参数在此处定义
         super().__init__()  # 必须先执行父类的初始化
         self.fc1 = t.nn.Linear(256, 128)
         self.fc2 = t.nn.Linear(128, 16)
         self.fc3 = t.nn.Linear(16, 3)
 
-    def forward(self, x):
-        # x的形状应为(batch_size, channels, height, width)
+    def forward(self, x):  # 激活函数在此处定义
+        # x的形状应为(batch_size, data_size)
         x = t.nn.ReLU()(self.fc1(x))
         x = t.nn.Sigmoid()(self.fc2(x))
         x = self.fc3(x)
         return x
 
 
+# 全连接网络，自定义初始化
+class FullConnectedNetV2(t.nn.Module):
+    def __init__(self):  # 参数在此处定义
+        super().__init__()  # 必须先执行父类的初始化
+        self.fc1 = t.nn.Linear(256, 128)
+        # 调用init模块赋值
+        init.xavier_normal_(self.fc1.weight)
+        init.constant_(self.fc1.bias, 0.0)
+        # 赋值给指定值
+        self.fc1.bias.data = t.tensor(np.ones((128, )), dtype=t.float32)  # dtype需要匹配
+        # self.fc1.bias.data = t.tensor(np.zeros((128, )), dtype=t.float64)
+        self.fc2 = t.nn.Linear(128, 16)  # 不初始化，直接使用内容中的随机数
+        self.fc3 = t.nn.Linear(16, 3)
+
+    def forward(self, x):  # 激活函数在此处定义
+        # x的形状应为(batch_size, data_size)
+        x = t.nn.ReLU()(self.fc1(x))
+        x = t.nn.Sigmoid()(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# 获取Module中信息的函数
+net = FullConnectedNetV2()
+for parameter in net.parameters():
+    # Module对象中的parameter全部打印出来，可以打印Sub-Module中的parameter
+    print(parameter)
+for name, parameter in net.named_parameters():
+    print(name)  # name为__init__()中定义的变量
+
 if __name__ == '__main__':
-    net = FullConnectedNet()
+    net = FullConnectedNetV2()
     input = t.randn(18, 256)
     out = net(input)
     print(out.size())
--- PyTorch实现模板/基本操作.py
+++ PyTorch实现模板/基本操作.py
@@ -6,14 +6,15 @@ x = t.Tensor(5, 3)
 x = t.tensor([3, 4, 5])
 a = np.random.rand(2, 3)
 x = t.tensor(a)  # 从numpy数组到Tensor，第一种方法
-x = t.from_numpy(a)  # 从numpy数组到Tensor，第二种方法
+x = t.from_numpy(a)  # 从numpy数组到Tensor，第二种方法，生成的为DoubleTensor，非默认的FloatTensor
+print(x.dtype)  # Double对应t.float64或t.double，其他类型依此类推
 a[0, 0] = 2.3  # a与x共享内存
 b = x.numpy()  # 从Tensor到numpy数组
 b[0, 0] = 2.3  # b与x共享内存
 
 # Tensor中值的修改
 x[0, 0] = 2.5
-x[0:2, 0:2] = t.tensor(np.ones((2, 2)))  # 一组值的修改必须用Tensor包起来
+x[0:2, 0:2] = t.tensor(np.ones((2, 2)))  # 一组值的修改必须用Tensor包起来，如此赋值不改变内存地址
 
 # 生成随机元素
 x = t.rand(5, 3)
@@ -38,3 +39,10 @@ z = t.add(x, y, out=z)  # 指定赋值给z
 
 y.add(x)  # y不改变
 y.add_(x)  # y改变，原位加法
+print(t.cuda.is_available())
+print(t.cuda.device_count())
+# Module在CUDA上的实现，要求相关的Tensor和Module都要CUDA实现
+x = t.rand(5, 4).cuda(t.device('cuda:1'))  # 指定CUDA设备
+x = t.rand(5, 4).cuda()
+fc = t.nn.Linear(4, 3).cuda()  # 模块中的参数会自动转移到CUDA中
+y = fc(x)
