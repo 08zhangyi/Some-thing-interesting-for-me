e53d852124f6c566ecb96890dcfe35b02d9a3d14
ccaipc <395871987@qq.com>
2019-09-12 16:22:40 +0800

    pytorch 学习

--- PyTorch学习/PyTorch深度学习/ch05/01.py
+++ PyTorch学习/PyTorch深度学习/ch05/01.py
@@ -2,6 +2,8 @@ import torch
 import torchvision
 import torch.utils.data
 import matplotlib.pyplot as plt
+import torch.nn
+import torch.nn.functional
 
 
 transformation = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))])
@@ -24,3 +26,45 @@ def plot_img(image):
 sample_data = next(iter(train_loader))
 plot_img(sample_data[0][1])
 plot_img(sample_data[0][2])
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = torch.nn.Dropout2d()
+        self.fc1 = torch.nn.Linear(320, 50)
+        self.fc2 = torch.nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = torch.nn.functional.relu(torch.nn.functional.max_pool2d(self.conv1(x), 2))
+        x = torch.nn.functional.relu(torch.nn.functional.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return torch.nn.functional.log_softmax(x)
+
+
+is_cuda = False
+
+
+def fit(epoch, model, data_loader, phase='training', volatile=False):
+    if phase == 'training':
+        model.train()
+    if phase == 'validation':
+        model.eval()
+        volatile = True
+    running_loss = 0.0
+    running_correct = 0
+    for batch_idx, (data, target) in enumerate(data_loader):
+        if is_cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = torch.tensor(data), torch.tensor(target)
+        if phase == 'training':
+            optimizer.zero_grad()
+        output = model(data)
+        loss = torch.nn.functional.nll_loss(output, target)
+        running_loss += torch.nn.functional.nll_loss(output, target, size_average=False).data[0]
+        preds = output.data.max(dim=1, keepdim=True)[1]
