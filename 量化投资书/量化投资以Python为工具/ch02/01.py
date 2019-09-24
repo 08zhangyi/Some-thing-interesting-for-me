2bbfbe802556c7b03956c38f90a6deb28873658d
ccaipc <395871987@qq.com>
2019-09-24 15:18:04 +0800

    python学习

---
+++ 量化投资书/量化投资以Python为工具/ch02/01.py
@@ -0,0 +1,13 @@
+print("Hello Python!")
+2 * 3
+
+tenInterger = [i for i in range(11)]
+print(tenInterger)
+for i in tenInterger:
+    if i%2 == 0:
+        print(i)
+
+stockValue = 30.5 * 3
+print('stock value equals to\
+print times amount')
+
---
+++ 量化投资书/量化投资以Python为工具/ch02/test2.py
@@ -0,0 +1,7 @@
+stockPrice1 = 76
+stockPrice2 = 80
+if stockPrice2 > stockPrice1:
+    print("Price is high!")
+else:
+    print("Price is low!")
+print(stockPrice2)
