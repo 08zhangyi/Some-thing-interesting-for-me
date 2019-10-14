b63b23e5e38adfae68898b4bd3581e18b25d1dca
ccaipc <395871987@qq.com>
2019-10-14 08:17:21 +0800

    python学习

---
+++ 量化投资书/量化投资以Python为工具/ch05/01.py
@@ -0,0 +1,66 @@
+a = 7; b = 8
+a.__add__(b)
+a.__sub__(b).__sub__(1)
+7 + 8
+3 + 2.0
+3 - 2.0
+3 * 2.0
+3 ** 2.0
+3 / 2.0
+print(3 // 2.0)
+3 % 2.0
+True + 2.0
+False // 2.0
+a = 2 + 3j
+b = 3 + 5j
+a + b
+'Python' + 'Quant'
+'Python' * 4
+'%s，很棒！' % ('python')
+
+a = 3
+b = 4
+b += a
+b
+
+a = 30
+b = 30
+a != b
+True == 1
+
+True and False
+False or True
+a = 32.5
+bool(a)
+print(a and 34)
+print(a or 34)
+not a
+b = 0
+bool(b)
+b and a
+b or 1
+False and True
+0 and 33
+
+a = 2
+b = 2
+a == b
+id(a)
+id(b)
+a is b
+a is not b
+b = 50
+id(b)
+id(a)
+a is b
+a is not b
+c = [2, 3, 4]
+id(c)
+d = [2, 3, 4]
+id(d)
+c is d
+c == d
+'a' is 'A'
+True is 1
+id(True)
+id(1)
