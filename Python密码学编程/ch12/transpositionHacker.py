ae772624837abd1a4d6f03ca816509e3cca080b6
ccaipc <395871987@qq.com>
2022-12-12 13:27:14 +0800

    python学习

---
+++ Python密码学编程/ch12/transpositionHacker.py
@@ -0,0 +1,42 @@
+import pyperclip, detectEnglish, transpositionDecrypt
+
+
+def main():
+    myMessage = """AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu thllnrshicwsg etriebruaisss  d iorr."""
+
+    hackedMessage = hackTransposition(myMessage)
+
+    if hackedMessage == None:
+        print('Failed to hack encryption.')
+    else:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+
+
+def hackTransposition(message):
+    print('Hacking...')
+
+    print('(Press Ctrl-C (on Windows) or Ctrl-D (on macOS and Linux) to quit at any time.)')
+
+    for key in range(1, len(message)):
+        print('Trying key #%s...' % (key))
+
+        decryptedText = transpositionDecrypt.decryptMessage(key, message)
+
+        if detectEnglish.isEnglist(decryptedText):
+            print()
+            print('Possible encryption hack:')
+            print('Key %s: %s' % (key, decryptedText[:100]))
+            print()
+            print('Enter D if done, anything else to continue hacking:')
+            response = input('> ')
+
+            if response.strip().upper().startswith('D'):
+                return decryptedText
+
+    return None
+
+
+if __name__ == '__main__':
+    main()
